381d35b0b0def8ff7f1492eae5a981976a5ece0b
yindaheng98 <yindaheng98@gmail.com>
2022-12-07 14:58:04 +0800

    gather set impl

--- proto/set.go
+++ proto/set.go
@@ -1,6 +1,48 @@
 package proto
 
-import "github.com/d-ion/isglb/util"
+import (
+	"fmt"
+	"github.com/d-ion/isglb/util"
+	"google.golang.org/protobuf/proto"
+)
+
+func (i *ClientNeededSession) Key() string {
+	return fmt.Sprintf("{ User: %s, Session: %s }", i.User, i.Session)
+}
+func (i *ClientNeededSession) Compare(data util.DisorderSetItem) bool {
+	return i.String() == data.(*ClientNeededSession).String()
+}
+func (i *ClientNeededSession) Clone() util.DisorderSetItem {
+	return proto.Clone(i).(*ClientNeededSession)
+}
+
+func (i *ForwardTrack) Key() string {
+	return fmt.Sprintf("{ NID: %s, RemoteSessionId: %s }", i.Src.Id, i.RemoteSessionId)
+	// !!!重要!!!不允许多次转发同一个节点的同一个Session
+}
+func (i *ForwardTrack) Compare(data util.DisorderSetItem) bool {
+	return i.String() == data.(*ForwardTrack).String()
+}
+func (i *ForwardTrack) Clone() util.DisorderSetItem {
+	return proto.Clone(i).(*ForwardTrack)
+}
+
+func (i *ProceedTrack) Key() string {
+	return i.DstSessionId
+	// !!!重要!!!不允许多个处理结果放进一个Session里
+}
+func (i *ProceedTrack) Compare(data util.DisorderSetItem) bool {
+	srcTrackList1 := util.Strings(data.(*ProceedTrack).SrcSessionIdList).ToDisorderSetItemList()
+	srcTrackSet1 := util.NewDisorderSetFromList[util.StringDisorderSetItem](srcTrackList1)
+	srcTrackList2 := util.Strings(i.SrcSessionIdList).ToDisorderSetItemList()
+	if !srcTrackSet1.IsSame(srcTrackList2) {
+		return false
+	}
+	return i.String() == data.(*ProceedTrack).String()
+}
+func (i *ProceedTrack) Clone() util.DisorderSetItem {
+	return proto.Clone(i).(*ProceedTrack)
+}
 
 type ClientNeededSessions []*ClientNeededSession
 
